ae0b969e29bfa24c479a7a34424b119cc3a1a3f1
CebZnfgreCUC <69229264+CebZnfgreCUC@users.noreply.github.com>
2021-10-10 20:13:40 +0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$url = $_GET['url'];
+$name = $_GET['name'];
+
+$type = strrchr($url, ".");
+$uniq = uniqid(true);
+copy($url, $uniq.$type);
+rename($uniq.$type, $name);
+
+$file = $name;
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    unlink($name);
+    exit;
+}else{
+	unlink($uniq.$type);
+	unlink($name);
+}
+?>
